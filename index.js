f628d2cd8c208e8823f509e5a224bbd7a7d8f69e
Cansu Aysagdic <cansu.aysagdic@ogr.iu.edu.tr>
2023-03-24 18:49:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return upperCaseCustomerName;
+}
+
+function setBestCustomer() {
+  var bestCustomer = 'not a bob';
+  return bestCustomer;
+}
+bestCustomer = setBestCustomer();
+
+function overwriteBestCustomer(param1) {
+  bestCustomer = param1;
+}
+//overwriteBestCustomer('Cansu');
+
+const leastFavoriteCustomer = 'John';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'Bob';
+  console.log(`The least favorite customer is ${leastFavoriteCustomer}`);
+}
+changeLeastFavoriteCustomer(); 
+
+
